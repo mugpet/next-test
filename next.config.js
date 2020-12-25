c47092bcbb3def9c6ae66f844feae0446b8465cf
Mugo Petersen <mugpet@gmail.com>
2020-12-25 14:35:10 +0100

    update again

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-const data = require('./utils/projectsData');
+// const data = require('./utils/projectsData');
 
 module.exports = {
   trailingSlash: true,
   exportPathMap: async function () {
-    const { projects } = data;
+    // const { projects } = data;
     const paths = {
       '/': { page: '/' },
     };
 
-    projects.forEach((project) => {
-      paths[`/project/${project.slug}`] = {
-        page: '/project/[path]',
-        query: { path: project.slug },
-      };
-    });
+    // projects.forEach((project) => {
+    //   paths[`/project/${project.slug}`] = {
+    //     page: '/project/[path]',
+    //     query: { path: project.slug },
+    //   };
+    // });
 
     return paths;
   },

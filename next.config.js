1e85bd59fd2a6524e617c63102f2884b78e4aacd
Mugo Petersen <mugpet@gmail.com>
2021-01-09 07:52:30 +0100

    proxy1

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 // next.config.js
-
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/apis/v1/:path*",
-//         destination: "https://coda.io/apis/v1/:path*",
-//       }
-//     ]
-//   },
-// }
-
-
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/apis/v1/:path*",
+        destination: "https://coda.io/apis/v1/:path*",
+      }
+    ]
+  },
+}
 
 // { source: "/api/:path*", destination: "http://localhost:8000/:path*" },
 

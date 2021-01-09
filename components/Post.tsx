1f979be7d17f08e32e37f543fffcd7b51a2287f2
Mugo Petersen <mugpet@gmail.com>
2021-01-09 10:30:03 +0100

    more news design

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,21 +1,25 @@
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown"
 // import styles from "../styles/Post.module.css"
 
-
 const Post = (props) => {
+  const imageView = props.images.map((i) => {
+    if (i["@type"] == "ImageObject") {
+      return <div className="max-h-96 overflow-hidden rounded-md"><img className="w-full" key={i.url} src={i.url} /></div>
+    }
+  })
 
-    const imageView = props.images.map(i => {
-        if (i['@type'] == "ImageObject") {
-            return <img key={i.url} src={i.url} />
-        }
-    })
-
-    return <div className="mt-12 font-serif">
-        <div className="text-gray-400 text-2xl">{props.Title}</div>
+  return (
+    <div className="mt-16 font-serif">
+      <div className="text-gray-400 text-2xl mb-6">{props.Title}</div>
+      <div>
         {imageView}
         {/* <div className={styles.main} ><ReactMarkdown>{props.Content}</ReactMarkdown></div> */}
-        <div className="text-lg mt-2" ><ReactMarkdown>{props.Content}</ReactMarkdown></div>
+        <div className="text-base mt-2 blue  text-gray-600 leading-7">
+          <ReactMarkdown>{props.Content}</ReactMarkdown>
+        </div>
+      </div>
     </div>
+  )
 }
 
 export default Post
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   purge:  ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
 
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+    }
   },
   variants: {
     extend: {},

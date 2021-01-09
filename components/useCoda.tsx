2f5b45892f2146ced0f36650ff3ab7e87d172fdc
Mugo Petersen <mugpet@gmail.com>
2021-01-09 14:24:36 +0100

    dsign -- pointing at SCCFF Public

--- components/useCoda.tsx
+++ components/useCoda.tsx
@@ -8,11 +8,13 @@ res = requests.get(uri, headers=headers).json()
 */
 
 const useCoda = () => {
-  const docId = "brFgLa5Oku"
+  // const docId = "brFgLa5Oku" // SCCFF siden
+  const docId = "BNDWEzfw9c"
   const token = "aadfc738-6992-4c05-a236-19b705da3cc2"
   const pageId = "canvas-rqbxVNMw6i"
   // const tableIdOrName = "grid-5L6bQvHD-F"
-  const tableIdOrName = "grid-cNXF4rxLjX"  // Nyheder
+  // const tableIdOrName = "grid-cNXF4rxLjX"  // Nyheder
+  const tableIdOrName = "grid-6hh5uaxut7"  // Nyheder - SCCFF PUBLIC
   const headers = {
     Authorization: `Bearer ${token}`,
     ContentType: "application/x-www-form-urlencoded",
